180c74113c7b1f71841c9eedbcd4c2408011b676
DatDonathan <jojo08082003@gmail.com>
2022-05-11 21:45:31 +0200

    Added basic demo device

--- synthi-sam-core/src/device.rs
+++ synthi-sam-core/src/device.rs
@@ -1,4 +1,4 @@
-use crate::{audio::{AudioPort, ProcessingInfo, SampleInfo}, midi::MidiMessage};
+use crate::{audio::{AudioPort, ProcessingInfo, SampleInfo}, Midi::MidiMessage};
 
 
 pub struct NamedAudioPort {
@@ -29,6 +29,32 @@ impl NamedAudioPort {
 
 }
 
+pub struct NamedMidiPort {
+    name: &'static str,
+    identifier: &'static str,
+}
+
+impl NamedMidiPort {
+    
+    pub fn new(name: &'static str, identifier: &'static str) -> NamedMidiPort {
+        return NamedMidiPort {
+            name: name,
+            identifier: identifier,
+        };
+    }
+
+    #[inline(always)]
+    pub fn get_name(&self) -> &'static str {
+        return self.name;
+    }
+
+    #[inline(always)]
+    pub fn get_identifier(&self) -> &'static str {
+        return self.identifier;
+    }
+
+}
+
 pub struct DeviceInfo {
     pub name: &'static str,
     pub type_identifier: &'static str,
@@ -40,19 +66,17 @@ pub trait Device {
 
     fn setup(&mut self, info: ProcessingInfo);
 
-    fn process(&mut self, info: SampleInfo) -> (f64, f64);
+    fn process(&mut self, info: SampleInfo);
 
     fn recieve_midi(&mut self, msg: MidiMessage, info: SampleInfo, port: usize);
 
-    fn audio_input_port(&mut self, index: usize) -> Result<&mut NamedAudioPort, &'static str>;
 
-    fn audio_input_port_count(&mut self, index: usize) -> Result<&mut NamedAudioPort, &'static str>;
+    fn audio_input_port(&mut self, index: usize) -> Result<&mut NamedAudioPort, &'static str>;
 
     fn audio_output_port(&mut self, index: usize) -> Result<&mut NamedAudioPort, &'static str>;
 
-    fn audio_output_port_count(&mut self, index: usize) -> Result<&mut NamedAudioPort, &'static str>;
+    fn midi_input_port(&mut self, index: usize) -> Result<&mut NamedMidiPort, &'static str>;
 
-
+    fn midi_output_port(&mut self, index: usize) -> Result<&mut NamedMidiPort, &'static str>;
 
 }
-
--- synthi-sam-stage/src/main.rs
+++ synthi-sam-stage/src/main.rs
@@ -1,3 +1,72 @@
+use synthi_sam_core::{audio::{AudioPort, ProcessingInfo}, device::{NamedMidiPort, DeviceInfo, NamedAudioPort}};
+
+struct DemoDevice {
+    info: DeviceInfo,
+    output: NamedAudioPort,
+    midiin: NamedMidiPort,
+    processing_info: ProcessingInfo,
+}
+
+impl Device {
+
+    fn new() -> DemoDevice {
+        return DemoDevice {
+            info: DeviceInfo {
+                name: "Demo Synth",
+                type_identifier: "synthi_sam_demo_synth"
+            }, 
+            output: NamedAudioPort::new("Stereo Out", "stereo_out", 2), 
+            midiin: NamedMidiPort::new("MIDI In", "midi_in"),
+            processing_info: ProcessingInfo::default(),
+        }
+    }
+
+}
+
+impl Device for DemoDevice {
+
+    fn info(&self) -> &DeviceInfo {
+        return &self.info;
+    }
+
+    fn setup(&mut self, info: ProcessingInfo) {
+        self.processing_info = info;
+        self.output.port.reset();
+    }
+
+    fn process(&mut self, info: SampleInfo) {
+
+    }
+
+    fn recieve_midi(&mut self, msg: MidiMessage, info: SampleInfo, port: usize) {
+
+    }
+
+    fn audio_input_port(&mut self, index: usize) -> Result<&mut NamedAudioPort, &'static str> {
+        return None;
+    }
+
+    fn audio_output_port(&mut self, index: usize) -> Result<&mut NamedAudioPort, &'static str> {
+        return match index {
+            0 => Some(&self.output),
+            _ => None,
+        }
+    }
+
+    fn midi_input_port(&mut self, index: usize) -> Result<&mut NamedMidiPort, &'static str> {
+        return match index {
+            0 => Some(&self.midiin),
+            _ => None,
+        }
+    }
+
+    fn midi_output_port(&mut self, index: usize) -> Result<&mut NamedMidiPort, &'static str> {
+        return None
+    }
+
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
